106dfb2915dbc6a59775f8de6d618a01755be06b
aytenselim <slmova.ayten@gmail.com>
2022-06-05 19:39:10 +0400

    commit2

--- src/components/common/Logo/index.jsx
+++ src/components/common/Logo/index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import logo from './logo.png';
+import logo from './food-shop-logo.jpg';
 import './styles.css';
 
 const Logo = () => (
   <div className='header-content_logo'>
     <div>
       <img src={logo} alt='logo' />
-      <span>
-        <b>Taste</b>
-      </span>
     </div>
-    <p>
-      <b>Restaurant & BBQ</b>
-    </p>
   </div>
 );
 
